fb8aa5dd66c19c52b31d5c5d8df8bc5c83547db5
chengyu.wang <hyxx2guo@gmail.com>
2019-09-24 17:00:33 +0800

    fix(delete): delete unuseful files

--- src/router/index.js
+++
@@ -1,8 +0,0 @@
-const nextRoutes = require('next-routes')
-const { routerConfig } = require('config/router')
-
-const routes = (module.exports = nextRoutes())
-
-if (Array.isArray(routerConfig)) {
-  routerConfig.forEach(r => routes.add(r.route, r.page))
-}
